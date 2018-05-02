ef5b3b445c4211c7a2211b7d073814c0728f2368
Hello Team Omar <helloteam.omar@gmail.com>
2018-05-02 11:45:22 -0500

    base de datos
    base de datos

--- HelloEncuesta/src/app/app.module.ts
+++ HelloEncuesta/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+//import { TasksService } from '../providers/tasks-service/tasks-service';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { SQLite } from '@ionic-native/sqlite';
+import { TasksServiceProvider } from '../providers/tasks-service/tasks-service';
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    SQLite,
+    TasksServiceProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TasksServiceProvider
   ]
 })
 export class AppModule {}
---
+++ HelloEncuesta/src/providers/tasks-service/tasks-service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SQLiteObject } from '@ionic-native/sqlite';
+
+/*
+  Generated class for the TasksServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class TasksServiceProvider {
+
+  constructor() {
+
+  }
+  setDatabase(db: SQLiteObject){
+    if(this.db === null){
+      this.db = db;
+    }
+  }
+  createTable(){
+  let sql = 'CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed INTEGER)';
+  return this.db.executeSql(sql, []);
+   }
+
+
+
+}

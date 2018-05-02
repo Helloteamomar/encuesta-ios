4a9eedd4a64abcdd9d10587a398038d28252d1f3
Mike <helloteam.mike@gmail.com>
2018-05-02 16:16:29 -0500

    Cambios
    Cambios

--- HelloEncuesta/src/pages/registro/registro.ts
+++ HelloEncuesta/src/pages/registro/registro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EncuestaPage } from '../encuesta/encuesta';
 
 /**
  * Generated class for the RegistroPage page.
@@ -20,6 +21,10 @@ export class RegistroPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegistroPage');
+  }
+
+  pEncuesta(){
+    this.navCtrl.setRoot(EncuestaPage);
   }
 
 }
